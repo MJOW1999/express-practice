4540d76790b06c78c5a16d21d66d1fa5658e89dd
MJOW1999 <michael.jow.williams@gmail.com>
2021-10-09 20:46:09 +0100

    Correctly link home.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
+const home = require("./routes/home.js");
+
+server.get("/", home.get);
 
 const PORT = process.env.PORT || 3000;
 
